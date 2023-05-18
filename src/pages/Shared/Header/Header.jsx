78d842167eced9f9ad03c6cd195792275127e163
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-05-18 13:24:54 +0600

    added login and register from layout

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Marquee from "react-fast-marquee";
-import { Nav, NavDropdown, NavLink, Navbar } from 'react-bootstrap';
+
 
 const Header = () => {
+
+    
+
     return (
         <Container className='mt-4'>
           <div className='text-center'>
@@ -20,24 +22,7 @@ const Header = () => {
   I can be a React component, multiple React components, or just some text.
 </Marquee>
           </div>
-          <Navbar collapseOnSelect variant='light' bg="light" expand="lg">
-      <Container>
-        
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Career</Nav.Link>
-
-          </Nav>
-          <NavLink href='#'>Profile</NavLink>
-          <NavLink >
-            <Button variant='secondary'>Login</Button>
-          </NavLink>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          
         </Container>
     );
 };
---
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+import { AuthContext } from '../../../providers/AuthProvider';
+
+const NavigationBar = () => {
+
+    const {user} = useContext(AuthContext);
+
+    return (
+        <Container>
+            <Navbar collapseOnSelect variant='light' bg="light" expand="lg">
+      <Container>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav className='mx-auto'>
+              <Link to='/'>Home</Link>
+            </Nav>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Career</Nav.Link>
+
+          </Nav>
+         {user && <Nav.Link href='#'><FaUserCircle style={{fontSize:'2rem'}}></FaUserCircle></Nav.Link>}
+          <Nav >
+            {user ? 
+            <Button variant='secondary'>LogOut</Button>:
+
+            <Link to='/login'>
+            <Button variant='secondary'>Login</Button>
+            </Link>
+            
+          }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </Container>
+    );
+};
+
+export default NavigationBar;
